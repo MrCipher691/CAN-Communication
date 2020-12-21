2616d8b684cfaffef05bb9749d72171a69cfff22
MrCipher691 <ayush.ram691@gmail.com>
2020-12-21 18:44:44 +0530

    Testing

--- Nano-MPU/src/main.cpp
+++ Nano-MPU/src/main.cpp
@@ -70,7 +70,7 @@ void setup()
   SPI.begin();               //Begins SPI communication
   
   mcp2515.reset();
-  mcp2515.setBitrate(CAN_50KBPS,MCP_8MHZ); //Sets CAN at speed 500KBPS and Clock 8MHz
+  mcp2515.setBitrate(CAN_500KBPS,MCP_8MHZ); //Sets CAN at speed 500KBPS and Clock 8MHz
   mcp2515.setNormalMode();
 
   Serial.println("Initialize MPU6050");
@@ -124,18 +124,12 @@ void loop()
   canMsg.can_id  = 0x036;           //CAN id as 0x036 
   canMsg.can_dlc = 8;               //CAN data length as 8
   canMsg.data[0] = canMsg.can_id;
-  /*canMsg.data[1] = Ax;
+  canMsg.data[1] = Ax;
   canMsg.data[2] = Ay;
   canMsg.data[3] = Az;
   canMsg.data[4] = Gx;
   canMsg.data[5] = Gy;
-  canMsg.data[6] = Gz;*/
-  canMsg.data[1] = "7DF";
-  canMsg.data[2] = "7DF";
-  canMsg.data[3] = "7DF";
-  canMsg.data[4] = "7DF";
-  canMsg.data[5] = "7DF";
-  canMsg.data[6] = "7DF";
+  canMsg.data[6] = Gz;
   canMsg.data[7] = 0x00;
   mcp2515.sendMessage(&canMsg);     //Sends the CAN message
 
